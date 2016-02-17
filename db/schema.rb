96ac35e5b34cc07776f9a57e938fc6bec63caf24
Andre Chalom <andrechalom@gmail.com>
2016-02-17 01:29:48 -0200

    Tentativa de gerar autorizacao

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216224428) do
+ActiveRecord::Schema.define(version: 20160217024546) do
 
   create_table "users", force: :cascade do |t|
     t.string   "login"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20160216224428) do
     t.boolean  "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "users", ["user_id"], name: "index_users_on_user_id"
 
 end
